d494215fd75c703ccae722b6cd77d9181089a70a
Cuong Le <cuong.le@ologn.tech>
2025-08-06 07:24:30 +0000

    color calibration test

---
+++ build/CMakeFiles/color_calibration_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/color_calibration_test.dir/src/main.cpp.o"
+  "CMakeFiles/color_calibration_test.dir/src/main.cpp.o.d"
+  "color_calibration_test"
+  "color_calibration_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/color_calibration_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
